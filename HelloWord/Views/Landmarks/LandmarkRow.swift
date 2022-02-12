3ff99fd0d8b1e982d80b4c761efc08ad438f5161
starpro123 <starpro123@naver.com>
2022-02-12 22:52:28 +0900

    3번째 튜토리얼 완성

--- HelloWord/Views/Landmarks/LandmarkRow.swift
+++ HelloWord/Views/Landmarks/LandmarkRow.swift
@@ -6,22 +6,35 @@
 //
 
 import SwiftUI
+import Combine
 
+class Rand: ObservableObject {
+    @Published var random = "\((1...1000).randomElement())"
+}
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+    @ObservedObject private var random = Rand()
+//    @StateObject private var random = Rand()
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
+//            Text(landmark.name)
+            Text(random.random)
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
